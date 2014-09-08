0e01682142e56ae19d796741016fcab0e032a146
Jack Poulson <jpoulson@cc.gatech.edu>
2014-09-08 16:40:28 -0700

    Switching license to the GPLv2 due to the current dependence upon NFFT3, which does not have a non-GPL'd alternative.

--- include/rt-lps-mri/core/environment_impl.hpp
+++ include/rt-lps-mri/core/environment_impl.hpp
@@ -1,10 +1,21 @@
 /*
-   Copyright (c) 2013, Jack Poulson, Ricardo Otazo, and Emmanuel Candes
+   Copyright (c) 2013-2014, Jack Poulson, Ricardo Otazo, and Emmanuel Candes
    All rights reserved.
  
-   This file is part of Real-Time Low-rank Plus Sparse MRI (RT-LPS-MRI) and is 
-   under the BSD 2-Clause License, which can be found in the LICENSE file in the
-   root directory, or at http://opensource.org/licenses/BSD-2-Clause
+   This file is part of Real-Time Low-rank Plus Sparse MRI (RT-LPS-MRI).
+
+   RT-LPS-MRI is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   RT-LPS-MRI is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with RT-LPS-MRI.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
 #ifndef RTLPSMRI_CORE_ENVIRONMENT_IMPL_HPP
